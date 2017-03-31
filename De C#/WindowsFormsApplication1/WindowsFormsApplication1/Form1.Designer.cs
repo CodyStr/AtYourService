8864ab0c395f10df1d6e15a6ff88c40f73e6386a
Cody Strijbosch <codystrij678@gmail.com>
2017-03-31 09:22:43 +0200

    init commit

---
+++ De C#/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTNForward = new System.Windows.Forms.Button();
+            this.BTNLeft = new System.Windows.Forms.Button();
+            this.BTNBackwards = new System.Windows.Forms.Button();
+            this.BTNRight = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.txtDistance = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTouch = new System.Windows.Forms.TextBox();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BTNForward
+            // 
+            this.BTNForward.Font = new System.Drawing.Font("Wingdings", 32F);
+            this.BTNForward.Location = new System.Drawing.Point(89, 24);
+            this.BTNForward.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BTNForward.Name = "BTNForward";
+            this.BTNForward.Size = new System.Drawing.Size(59, 64);
+            this.BTNForward.TabIndex = 0;
+            this.BTNForward.Text = "á";
+            this.BTNForward.UseVisualStyleBackColor = true;
+            this.BTNForward.Click += new System.EventHandler(this.BTNForward_Click);
+            // 
+            // BTNLeft
+            // 
+            this.BTNLeft.Font = new System.Drawing.Font("Wingdings", 32F);
+            this.BTNLeft.Location = new System.Drawing.Point(35, 80);
+            this.BTNLeft.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BTNLeft.Name = "BTNLeft";
+            this.BTNLeft.Size = new System.Drawing.Size(56, 58);
+            this.BTNLeft.TabIndex = 1;
+            this.BTNLeft.Text = "ß";
+            this.BTNLeft.UseVisualStyleBackColor = true;
+            this.BTNLeft.Click += new System.EventHandler(this.BTNLeft_Click);
+            // 
+            // BTNBackwards
+            // 
+            this.BTNBackwards.Font = new System.Drawing.Font("Wingdings", 32F);
+            this.BTNBackwards.Location = new System.Drawing.Point(89, 80);
+            this.BTNBackwards.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BTNBackwards.Name = "BTNBackwards";
+            this.BTNBackwards.Size = new System.Drawing.Size(59, 58);
+            this.BTNBackwards.TabIndex = 2;
+            this.BTNBackwards.Text = "â";
+            this.BTNBackwards.UseVisualStyleBackColor = true;
+            this.BTNBackwards.Click += new System.EventHandler(this.BTNBackwards_Click);
+            // 
+            // BTNRight
+            // 
+            this.BTNRight.Font = new System.Drawing.Font("Wingdings", 32F);
+            this.BTNRight.Location = new System.Drawing.Point(146, 80);
+            this.BTNRight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BTNRight.Name = "BTNRight";
+            this.BTNRight.Size = new System.Drawing.Size(59, 58);
+            this.BTNRight.TabIndex = 3;
+            this.BTNRight.Text = "à";
+            this.BTNRight.UseVisualStyleBackColor = true;
+            this.BTNRight.Click += new System.EventHandler(this.BTNRight_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(16, 181);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 15;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(210, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 15;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // txtDistance
+            // 
+            this.txtDistance.Location = new System.Drawing.Point(350, 36);
+            this.txtDistance.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDistance.Multiline = true;
+            this.txtDistance.Name = "txtDistance";
+            this.txtDistance.Size = new System.Drawing.Size(57, 21);
+            this.txtDistance.TabIndex = 5;
+            this.txtDistance.Text = "TextBlock";
+            this.txtDistance.TextChanged += new System.EventHandler(this.txtDistance_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(262, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Afstand sensor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(285, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Druk knop";
+            // 
+            // txtTouch
+            // 
+            this.txtTouch.Location = new System.Drawing.Point(350, 80);
+            this.txtTouch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTouch.Multiline = true;
+            this.txtTouch.Name = "txtTouch";
+            this.txtTouch.Size = new System.Drawing.Size(57, 21);
+            this.txtTouch.TabIndex = 8;
+            this.txtTouch.Text = "TextBlock";
+            this.txtTouch.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(350, 124);
+            this.txtColor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtColor.Multiline = true;
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(57, 22);
+            this.txtColor.TabIndex = 9;
+            this.txtColor.Text = "TextBlock";
+            this.txtColor.TextChanged += new System.EventHandler(this.txtColor_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(262, 127);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Kleuren sensor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 320);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.txtTouch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDistance);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.BTNRight);
+            this.Controls.Add(this.BTNBackwards);
+            this.Controls.Add(this.BTNLeft);
+            this.Controls.Add(this.BTNForward);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTNForward;
+        private System.Windows.Forms.Button BTNLeft;
+        private System.Windows.Forms.Button BTNBackwards;
+        private System.Windows.Forms.Button BTNRight;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox txtDistance;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTouch;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ De C#/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using Lego.Ev3.Core;
+using Lego.Ev3.Desktop;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Brick brick;
+        ColorSensorColor currentcolor;
+        int forward = 15;
+        int backward = -15;
+        uint time = 300;
+        ToolTip toolTip1 = new ToolTip();
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+            brick.Ports[InputPort.Three].SetMode(ColorMode.Color);
+        }
+
+        private async void init()
+        {
+            brick = new Brick(new BluetoothCommunication("COM3"));
+            brick.BrickChanged += brickChanged;
+            await brick.ConnectAsync();
+            await brick.DirectCommand.PlayToneAsync(5, 1000, 300);
+            await brick.DirectCommand.SetMotorPolarity(OutputPort.B | OutputPort.C, Polarity.Backward);
+            await brick.DirectCommand.StopMotorAsync(OutputPort.All, false);
+        }
+        private async void brickChanged(object o, BrickChangedEventArgs e)
+        {
+            txtDistance.Text = e.Ports[InputPort.Four].SIValue.ToString();
+            txtTouch.Text = e.Ports[InputPort.One].SIValue.ToString();
+            txtColor.Text = ((ColorSensorColor)e.Ports[InputPort.Three].SIValue).ToString();
+            this.currentcolor = (ColorSensorColor) e.Ports[InputPort.Three].SIValue;
+            if (this.currentcolor == ColorSensorColor.Blue)
+            {
+                await brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.B, 100, 500, false);
+                await brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.C, -25, 500, false);
+            }
+        }
+
+        private async void BTNForward_Click(object sender, EventArgs e)
+        {
+           if (this.currentcolor != ColorSensorColor.Red)
+                await brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.A | OutputPort.B | OutputPort.C, forward, time, false);
+        }
+
+        private async void BTNLeft_Click(object sender, EventArgs e)
+        {
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.B, backward, time, false);
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.C, forward, time, false);
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.A, forward, time, false);
+            await brick.BatchCommand.SendCommandAsync();
+        }
+
+        private async void BTNBackwards_Click(object sender, EventArgs e)
+        {
+            await brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.A | OutputPort.B | OutputPort.C, backward, time, false);
+        }
+
+        private async void BTNRight_Click(object sender, EventArgs e)
+        {
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.C, forward, time, false);
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.B, backward, time, false);
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.A, forward, time, false);
+            await brick.BatchCommand.SendCommandAsync();
+        }
+
+        public async void up()
+        {
+            if (this.currentcolor != ColorSensorColor.Red)
+                await brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.A | OutputPort.B | OutputPort.C, forward, time, false);
+        }
+        public async void left()
+        {
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.B, backward, time, false);
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.C, forward, time, false);
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.A, forward, time, false);
+            await brick.BatchCommand.SendCommandAsync();
+        }
+
+        public async void right()
+        {
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.C, backward, time, false);
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.B, forward, time, false);
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.A, forward, time, false);
+            await brick.BatchCommand.SendCommandAsync();
+        }
+
+        public async void down()
+        {
+            await brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.A | OutputPort.B | OutputPort.C, backward, time, false);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            
+            if (keyData == Keys.Up)
+            {
+                up();
+                return true;
+            }
+            
+            if (keyData == Keys.Down)
+            {
+                down();
+                return true;
+            }
+            
+            if (keyData == Keys.Left)
+            {
+                left();
+                return true;
+            }
+            
+            if (keyData == Keys.Right)
+            {
+                right();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            forward = trackBar1.Value;
+            backward = trackBar1.Value - (trackBar1.Value * 2);
+            toolTip1.SetToolTip(trackBar1, trackBar1.Value.ToString());
+        }
+
+        private void txtDistance_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtColor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
